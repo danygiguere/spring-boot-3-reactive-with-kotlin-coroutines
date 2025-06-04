a48fc4ef886fa1eb0b471c1f51c3b8d4a391122a
Dany Giguere <danygiguere23@gmail.com>
2025-06-04 16:21:01 -0400

    updated to CoWebFilter

--- src/main/kotlin/com/example/demo/configuration/WebFilterConfig.kt
+++ src/main/kotlin/com/example/demo/configuration/WebFilterConfig.kt
@@ -3,29 +3,26 @@ package com.example.demo.configuration
 import com.example.demo.app.post.PostService
 import com.example.demo.filters.checkPostOwnershipWebFilter
 import com.example.demo.filters.setLocaleWebFilter
-import kotlinx.coroutines.reactive.awaitFirstOrNull
-import kotlinx.coroutines.reactor.mono
 import org.springframework.stereotype.Component
+import org.springframework.web.server.CoWebFilter
+import org.springframework.web.server.CoWebFilterChain
 import org.springframework.web.server.ServerWebExchange
-import org.springframework.web.server.WebFilter
-import org.springframework.web.server.WebFilterChain
-import reactor.core.publisher.Mono
 
 
 @Component
 class WebFilterConfig(
     private val postService: PostService
-) : WebFilter {
-    override fun filter(exchange: ServerWebExchange, chain: WebFilterChain): Mono<Void> = mono {
-
+) : CoWebFilter() {
+    override suspend fun filter(
+        exchange: ServerWebExchange,
+        chain: CoWebFilterChain
+    ) {
         setLocaleWebFilter(exchange)
-
         val path = exchange.request.path
         val method = exchange.request.method
-
         if (!checkPostOwnershipWebFilter(path, method, exchange, postService)) {
-            return@mono null
+            return
         }
-        chain.filter(exchange).awaitFirstOrNull()
+        chain.filter(exchange)
     }
 }
