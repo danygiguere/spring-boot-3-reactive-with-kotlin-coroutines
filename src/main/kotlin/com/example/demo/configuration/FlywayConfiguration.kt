bae8941295f9201c03b83e91567bb60e4f39366d
Dany Giguere <danygiguere23@gmail.com>
2025-08-01 15:12:19 -0400

    improved secrets

--- src/main/kotlin/com/example/demo/configuration/FlywayConfiguration.kt
+++ src/main/kotlin/com/example/demo/configuration/FlywayConfiguration.kt
@@ -1,23 +1,28 @@
 package com.example.demo.configuration
 
 import org.flywaydb.core.Flyway
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.env.Environment
 
 @Configuration
-class FlywayConfiguration(private val env: Environment) {
+class FlywayConfiguration(
+    @Value("\${spring.flyway.url}") private val url: String,
+    @Value("\${spring.flyway.user}") private val user: String,
+    @Value("\${spring.flyway.password}") private val password: String,
+    @Value("\${spring.flyway.clean-disabled}") private val cleanDisabled: String
+) {
 
     @Bean(initMethod = "migrate")
     fun flyway(): Flyway {
         return Flyway.configure()
-                .baselineOnMigrate(true)
-                .dataSource(
-                        env.getRequiredProperty("spring.flyway.url"),
-                        env.getRequiredProperty("spring.flyway.user"),
-                        env.getRequiredProperty("spring.flyway.password")
-                )
-                .cleanDisabled(env.getProperty("spring.flyway.clean-disabled", Boolean::class.java, true))
-                .load()
+            .baselineOnMigrate(true)
+            .dataSource(
+                url,
+                user,
+                password
+            )
+            .cleanDisabled(cleanDisabled.toBoolean())
+            .load()
     }
 }
